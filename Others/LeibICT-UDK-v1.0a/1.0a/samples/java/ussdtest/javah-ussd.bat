24fd635d8307ad398758ded75ae09940cfed691e
Harshad Pardeshi <pardeshi.h@husky.neu.edu>
2018-03-26 18:04:56 -0400

    remove unwanted files

--- Others/LeibICT-UDK-v1.0a/1.0a/samples/java/ussdtest/javah-ussd.bat
+++
@@ -1,4 +0,0 @@
-@SET JDKPATH="C:\Program Files\Java\jdk1.6.0\bin"
-
-%JDKPATH%\javah -o USSD.h  -classpath ".\build\classes" -jni com.leibict.ussd.USSD
-@pause
--- Others/LeibICT-UDK-v1.0a/1.0a/samples/java/ussdtest/runUSSDTest.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
-java -jar USSDTest.jar
-#ln -s libussdjni.so.1.0 libussdjni.so
